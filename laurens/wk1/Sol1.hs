69e0328ec5e97f0bd741aabe7dc1dbbbe20c1eb4
laurensknoll <laurensknoll@gmail.com>
2013-09-08 14:43:31 +0200

    Finished exercises for chapter 1.

--- laurens/wk1/Sol1.hs
+++ laurens/wk1/Sol1.hs
@@ -2,21 +2,24 @@ module Sol1 where
 
 import GS
 
+-- 1.9
 mxmInt :: [Int] -> Int
 mxmInt [] = error "empty list"
 mxmInt [x] = x
 mxmInt (x:xs) = max x (mxmInt xs)
 
-removeFst :: Integral => a -> [a] -> [a]
+-- 1.10
+removeFst :: Int -> [Int] -> [Int]
 removeFst n [] = []
 removeFst n (x:xs) | x == n    = xs
                    | otherwise = x : (removeFst n xs)
 
-removeFstString :: String -> [String] -> [a]
+removeFstString :: String -> [String] -> [String]
 removeFstString s [] = []
 removeFstString s (x:xs) | x == s = xs
                          | otherwise = x : (removeFstString s xs)
 
+-- 1.11
 srtInts :: [Int] -> [Int]
 srtInts [] = []
 srtInts xs = m : (srtInts (removeFst m xs)) where m = mnmInt xs
@@ -26,11 +29,13 @@ srtInts' [] = []
 srtInts' xs = let m = mnmInt xs
               in m : (srtInts' (removeFst m xs))
 
+-- 1.13
 count :: Char -> String -> Int
 count a []     = 0
 count a (x:xs) | a == x    = 1 + (count a xs)
                | otherwise = (count a xs)
 
+-- 1.14
 blowup :: String -> String
 blowup [] = []
 blowup xs = blowup' (zip xs [1..])
@@ -39,12 +44,22 @@ blowup' :: [(Char, Int)] -> String
 blowup' []          = []
 blowup' ((c, n):cs) = replicate n c ++ blowup' cs
 
-mnmString :: [String] -> String
-mnmString [] = error "empty list"
-mnmString [x] = x
-mnmString (x:xs) = min x (mnmString xs)
-
+-- 1.15
 srtString :: [String] -> [String]
 srtString []     = []
-srtString xs = let x = mnmString xs
-               in x : (srtString (removeFstString xs))
+srtString (x:xs) = srtString (filter (<x) xs) ++ [x] ++ srtString (filter (>= x) xs)
+
+-- 1.17
+substring :: String -> String -> Bool
+susbstring [] ys = False
+substring xs [] = False
+substring xs ys | prefix xs ys || substring xs (tail ys) = True
+                | otherwise = False
+
+-- 1.20
+lengths :: [[a]] -> [Int]
+lengths xss = map length xss
+
+-- 1.21
+sumLengths :: [[a]] -> Int
+sumLengths xss = sum (lengths xss)
