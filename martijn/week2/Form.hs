ed3ec3d59b9307bcf01a7a2866730f278dcc9d3c
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-09-12 21:08:50 +0200

    Finished part 1

---
+++ martijn/week2/Form.hs
@@ -0,0 +1,6 @@
+module Form
+where
+
+import Week2
+
+
--- martijn/week2/Triangle.hs
+++ martijn/week2/Triangle.hs
@@ -10,6 +10,7 @@ data Shape = NoTriangle         -- sum of two sides lesser or equal than the thi
            | Other              -- the former rules do not apply
              deriving (Eq,Show)
 
+-- determine the triangle type from the length of three sides a,b,c
 triangle :: Integer -> Integer -> Integer -> Shape
 triangle a b c = triangle' (sort [a,b,c])
            where triangle' [a,b,c] | a + b <= c             = NoTriangle
@@ -19,22 +20,29 @@ triangle a b c = triangle' (sort [a,b,c])
                                    | otherwise              = Other
 
 -- Test cases
+--
+-- The test cases listed here are set up to test the (formal) definition of the
+-- triangle types. The triangle types have properties as listed beside them in
+-- the declaration of these types.
+-- The properties are used to create 3-tuples that satisfy these properties.
+-- Then these 3-tuples are passed to the test function to assert that it holds.
+
 equilaterals = [(1,1,1),(5,5,5),(1024,1024,1024)]
 rectangulars = [(3,4,5),(5,12,13),(8,15,17),(7,24,25),(68,285,293)]
 isosceles    = [(3,3,4),(3,4,3),(4,3,3),(100,1,100)]
 notriangles  = [(2,2,4),(283,495,1000),(1,2,3),(6,2,1)]
 others       = [(19,12,18),(2^63,2^7,2^63+1)]
 
--- expand 3-tuple to separate function arguments
-uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
-uncurry3 fn = \(a,b,c) -> fn a b c
-
--- list of all test cases
+-- combined list of all test cases
 allTests = [(equilaterals,Equilateral),
             (isosceles,Isosceles),
             (rectangulars,Rectangular),
             (notriangles,NoTriangle),
             (others,Other)]
+
+-- expand 3-tuple to separate function arguments
+uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+uncurry3 fn = \(a,b,c) -> fn a b c
 
 -- test function
 test :: [(Integer,Integer,Integer)] -> Shape -> Bool
