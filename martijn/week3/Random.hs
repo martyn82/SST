6c0c7b7aa530d13ee9876c51b6f36cd01f2a930e
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-09-17 22:00:21 +0200

    Updated some of permutation tests

--- martijn/week3/Random.hs
+++ martijn/week3/Random.hs
@@ -1,6 +1,7 @@
 module Random
 where
 
+import Control.Monad
 import Data.List
 import System.Random
 import Techniques
@@ -8,20 +9,31 @@ import Week3
 
 import CNF
 
-import System.IO.Unsafe
-
 -- generates a list of random integers
-genIntList :: [Int]
-genIntList = genIntList' 0 []
-       -- the following works, but its wrong... *unsafe* is unintentionally changing the return type from IO [Int] to [Int]
-       where genIntList' n ls | n < 3     = genIntList' (succ n) ((unsafePerformIO $ getRandomInt 10) : ls)
-                              | otherwise = ls
+genIntList :: IO [Int]
+genIntList = mapM (\ i -> getRandomInt 10) [1..3]
 
 -- determines whether ys is a permutation of xs
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation xs ys = any (\ perm -> perm == ys) (permutations xs)
 
+-- testable properties of isPermutation
+-- a permutation of a list is in fact the ordered set of unique elements of the list.
+
+permutationtests = [([1,2,3], [3,2,1]),
+                    ([1,1,1], [1,1,1]),
+                    ([1,2,3], [2,3,4]),
+                    ([10,20,30,405,1], [10,405,1,20,30])]
+
+pair :: (Monad m) => m [a] -> m [b] -> m ([a], [b])
+pair = liftM2 (,)
+
+-- tests the isPermutation function
+-- a permutation of x is every ordered list of unique elements of x
+--testIsPermutation :: Bool
+--testIsPermutation = and (mapM (\ (x, y) -> (isPermutation x y) == (isSameSet x y)) gentests)
+--              where isSameSet x y = ((nub (sort x)) == (nub (sort y)))
+--                    gentests      = mapM (\ i -> (pair genIntList genIntList)) [1]
+
+
 -- TODO Define some testable properties for the isPermutation function, and use your random generator for integer lists above to test isPermutation
--- Why not done so? The IO is causing problems.
-
-
