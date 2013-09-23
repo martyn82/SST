dd74022a12d9a8da3fb1c8e04a81dfc031a72681
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-09-23 21:05:02 +0200

    Added Set operations

--- martijn/week4/Set.hs
+++ martijn/week4/Set.hs
@@ -1,6 +1,7 @@
 module Set
 where
 
+import Data.List (intersect, (\\))
 import System.Random
 
 import SetOrd
@@ -27,4 +28,15 @@ genRandomSet = do
                 1 -> do
                     ls <- genIntList
                     return (list2set ls)
+
+
+-- unionSet already defined in SetOrd module
+
+-- intersectSet gives the intersection of two Sets
+intersectSet :: (Ord a) => Set a -> Set a -> Set a
+intersectSet (Set xs) (Set ys) = list2set (intersect xs ys)
+
+-- diffSet gives the non-associative difference between two sets
+diffSet :: (Ord a) => Set a -> Set a -> Set a
+diffSet (Set xs) (Set ys) = list2set (xs \\ ys)
 
