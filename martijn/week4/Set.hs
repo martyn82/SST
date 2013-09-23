18a24462baa66b1615f3bf132c15aada0b761ef9
Martijn Endenburg <martijn.endenburg@gmail.com>
2013-09-23 21:38:19 +0200

    Added tests for set operations

--- martijn/week4/Set.hs
+++ martijn/week4/Set.hs
@@ -40,3 +40,53 @@ intersectSet (Set xs) (Set ys) = list2set (intersect xs ys)
 diffSet :: (Ord a) => Set a -> Set a -> Set a
 diffSet (Set xs) (Set ys) = list2set (xs \\ ys)
 
+-- the union set of two sets A and B contains all elements of A and all elements of B
+unionProperty :: (Ord a) => Set a -> Set a -> Set a -> Bool
+unionProperty (Set xs) (Set ys) setZ = (all (\ x -> (inSet x setZ)) xs) && (all (\ y -> (inSet y setZ)) ys)
+
+-- tests the union function on sets
+testUnion :: Int -> IO ()
+testUnion 0 = do print ("all tests passed")
+testUnion n = do
+            s1 <- genRandomSet
+            s2 <- genRandomSet
+            let s = (unionSet s1 s2)
+            if (unionProperty s1 s2 s) then do
+                print ("test passed on: " ++ show s1 ++ " UNION " ++ show s2 ++ " = " ++ show s)
+                testUnion (n-1)
+            else
+                error ("test failed on: " ++ show s1 ++ " UNION " ++ show s2 ++ " = " ++ show s)
+
+-- the intersection of two sets A and B is the set that contains all elements that are elements of both A and B
+intersectProperty :: (Ord a) => Set a -> Set a -> Set a -> Bool
+intersectProperty setX setY (Set zs) = (all (\ z -> (inSet z setX) && (inSet z setY)) zs)
+
+-- tests the intersection function on sets
+testIntersect :: Int -> IO ()
+testIntersect 0 = do print ("all tests passed")
+testIntersect n = do
+                s1 <- genRandomSet
+                s2 <- genRandomSet
+                let s = (intersectSet s1 s2)
+                if (intersectProperty s1 s2 s) then do
+                    print ("test passed on: " ++ show s1 ++ " INTERSECT " ++ show s2 ++ " = " ++ show s)
+                    testIntersect (n-1)
+                else
+                    error ("test failed on: " ++ show s1 ++ " INTERSECT " ++ show s2 ++ " = " ++ show s)
+
+-- the difference of two sets A and B is the set of all elements of A that are not elements of B
+diffProperty :: (Ord a) => Set a -> Set a -> Set a -> Bool
+diffProperty (Set xs) setY (Set zs) = (filter (\ x -> (not (inSet x setY))) xs) == zs
+
+-- tests the difference function on sets
+testDiff :: Int -> IO ()
+testDiff 0 = do print ("all tests passed")
+testDiff n = do
+            s1 <- genRandomSet
+            s2 <- genRandomSet
+            let s = (diffSet s1 s2)
+            if (diffProperty s1 s2 s) then do
+                print ("test passed on: " ++ show s1 ++ " - " ++ show s2 ++ " = " ++ show s)
+                testDiff (n-1)
+            else
+                error ("test failed on: " ++ show s1 ++ " - " ++ show s2 ++ " = " ++ show s)
