38f0ccb509cdc23247abd1cdd4714df9c19176a6
laurensknoll <laurensknoll@gmail.com>
2013-09-25 09:23:29 +0200

    Added random set generator with arguments for length and min/max-value

--- laurens/wk4/Lab4.hs
+++ laurens/wk4/Lab4.hs
@@ -14,7 +14,23 @@ genIntList :: Int -> IO [Int]
 genIntList n = sequence $ (take n stream)
                    where stream = getRandomInt : stream
 
+-- getRandomInt' ((#minValue, #maxValue))
+getRandomInt' :: (Int, Int) -> IO Int
+getRandomInt' n = getStdRandom (randomR n)
+
+-- genIntList' (#lists) ((#minValue, #maxValue))
+genIntList' :: Int -> (Int, Int) -> IO [Int]
+genIntList' n m = sequence $ (take n stream)
+                      where stream = (getRandomInt' m) : stream
+
 -- gets a random set with max 10 elements (depends on the randomness of genIntList)
 getRandomSet :: IO (Set Int)
 getRandomSet = do xs <- genIntList 10
                   return (list2set xs)
+
+-- getRandomSet' (#max length of set) ((#minValue, #maxValue))
+getRandomSet' :: Int -> (Int, Int) -> IO (Set Int)
+getRandomSet' ml mv = do xs <- genIntList' ml mv
+                         return (list2set xs)
+
+-- Exercise 3:
